9025d4cf664af27ed9e81212dc7ea25686d73c61
Abhijeet Singh <abhijeet842799@gmail.com>
2023-08-03 23:48:35 +0530

    accordian, appbar, alert, backdrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 // import MuiTypography  from "./components/MuiTypography"
 // import MuiButton from "./components/MuiButton"
+// import SimpleBackdrop from "./components/MuiBackdrop"
+import BasicAccordion from "./components/MuiAccordian"
+import { ControlledAccordions } from "./components/MuiAccordian"
+import BasicAlerts from "./components/MuiAllert"
+import ResponsiveAppBar from "./components/MuiAppBar"
+import { BottomAppBar } from "./components/MuiAppBar"
 function App() {
 
 
   return (
     <>
+    <ResponsiveAppBar/>
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
+      {/* <SimpleBackdrop/> */}
+      <BasicAccordion/>
+      <ControlledAccordions/>
+      <BasicAlerts/>
+      <BottomAppBar/>
       
     </>
   )
